c26b85b7f468b1ebfd175e618c401f5fffad6eda
Dan <headhertz88@gmail.com>
2019-08-13 16:12:45 +0100

    Blog post styles/template - still needs CSS work

--- src/templates/post.jsx
+++ src/templates/post.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { graphql } from 'gatsby';
 import Helmet from 'react-helmet';
+import { Container, Row, Col, Button } from 'react-bootstrap';
 
 import MainLayout from '../layout';
 import { MetaData } from '../components/meta';
@@ -12,31 +13,37 @@ const Post = ( { data, location } ) => {
 
 	return (
 		<React.Fragment>
-			<MetaData data={data} location={location} type="article" />
-			<TitleSection location={location} sub={{ slug: 'blog', label: 'Blog' }} crumbLabel={post.title} />
-			<Helmet>
-				<style type="text/css">{`${post.codeinjection_styles}`}</style>
-			</Helmet>
-
 			<MainLayout>
-				<div className="container">
-					<article className="content">
-						{post.feature_image ? (
-							<figure className="post-feature-image">
-								<img src={post.feature_image} alt={post.title} />
-							</figure>
-						) : null}
-						<section className="post-full-content">
-							<h1 className="content-title">{post.title}</h1>
+				<MetaData data={data} location={location} type="article" />
+				{/* <TitleSection location={location} sub={{ slug: 'blog', label: 'Blog' }} crumbLabel={post.title} /> */}
+				{/* <Helmet>
+					<style type="text/css">{`${post.codeinjection_styles}`}</style>
+				</Helmet> */}
 
-							{/* The main post content */}
-							<section
-								className="content-body load-external-scripts"
-								dangerouslySetInnerHTML={{ __html: post.html }}
-							/>
-						</section>
-					</article>
-				</div>
+				<Container>
+					<Row className="row-centered">
+						<Col lg={10} md={10}>
+							<article className="post-content">
+								{post.feature_image ? (
+									<figure className="post-feature-image">
+										<img src={post.feature_image} alt={post.title} />
+									</figure>
+								) : null}
+								<section className="post-full-content">
+									<h1 className="content-title">
+										<strong>{post.title}</strong>
+									</h1>
+
+									{/* The main post content */}
+									<section
+										className="content-body load-external-scripts"
+										dangerouslySetInnerHTML={{ __html: post.html }}
+									/>
+								</section>
+							</article>
+						</Col>
+					</Row>
+				</Container>
 			</MainLayout>
 		</React.Fragment>
 	);
