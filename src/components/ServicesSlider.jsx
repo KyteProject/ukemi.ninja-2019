721445323060c034dc60ec51ab7c1fde070a7f80
Daniel Smith <headhertz88@gmail.com>
2019-12-03 12:39:10 +0000

    service slider temp

--- src/components/ServicesSlider.jsx
+++ src/components/ServicesSlider.jsx
@@ -1,23 +1,85 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
 import Slider from 'react-slick';
-import PostSliderCard from './PostSliderCard';
 
 export const ServicesSlider = () => {
 	const settings = {
 		arrows: false,
-		dots: true,
+		dots: false,
 		infinite: true,
 		speed: 500,
-		slidesToShow: 3,
-		slidesToScroll: 3
+		slidesToShow: 6,
+		slidesToScroll: 1
 	};
 
 	return (
-		<section className="recent-posts">
-			{/* img */}
-			<Container>
-				<Slider className="recent-slider" {...settings} />
+		<section className="section-services-slider">
+			<Container fluid className="text-center">
+				<div className="block-title">
+					<h2 className="block-title-text">Discover our services</h2>
+				</div>
+				<Slider className="services-slider" {...settings}>
+					<div className="service-item">
+						<div className="service-box">
+							<div className="service-icon">
+								<i className="kipso-icon-targeting" />
+							</div>
+							<h3 className="service-box-title">
+								<a href="#">Park Design</a>
+							</h3>
+						</div>
+					</div>
+					<div className="service-item">
+						<div className="service-box">
+							<div className="service-icon">
+								<i className="kipso-icon-targeting" />
+							</div>
+							<h3 className="service-box-title">
+								<a href="#">Bespoke Equipment</a>
+							</h3>
+						</div>
+					</div>
+					<div className="service-item">
+						<div className="service-box">
+							<div className="service-icon">
+								<i className="kipso-icon-targeting" />
+							</div>
+							<h3 className="service-box-title">
+								<a href="#">Event Planning</a>
+							</h3>
+						</div>
+					</div>
+					<div className="service-item">
+						<div className="service-box">
+							<div className="service-icon">
+								<i className="kipso-icon-targeting" />
+							</div>
+							<h3 className="service-box-title">
+								<a href="#">Consultation</a>
+							</h3>
+						</div>
+					</div>
+					<div className="service-item">
+						<div className="service-box">
+							<div className="service-icon">
+								<i className="kipso-icon-targeting" />
+							</div>
+							<h3 className="service-box-title">
+								<a href="#">Presentations</a>
+							</h3>
+						</div>
+					</div>
+					<div className="service-item">
+						<div className="service-box">
+							<div className="service-icon">
+								<i className="kipso-icon-targeting" />
+							</div>
+							<h3 className="service-box-title">
+								<a href="#">Workshops</a>
+							</h3>
+						</div>
+					</div>
+				</Slider>
 			</Container>
 		</section>
 	);
