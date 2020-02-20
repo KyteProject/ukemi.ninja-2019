7ec47d26cb32ac87f56a899b597d24d3959713e8
Daniel Smith <headhertz88@gmail.com>
2020-02-20 16:54:39 +0000

    footer fixed

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link, useStaticQuery, graphql } from "gatsby";
-import { Nav, Container } from "react-bootstrap";
+import { Nav, Container, Row, Col, Image } from "react-bootstrap";
 
 import config from "../../../data/siteConfig";
 
@@ -28,85 +28,129 @@ const Footer = () => {
 
   return (
     <footer className="footer">
-      <Container>
-        <Nav className="footer-nav">
-          <div className="footer-col footer-col-intro">
-            <a className="footer-logo" href="/" data-footer-link="1">
-              <img src="/logos/kanji-text-white2.svg" alt="Ukemi Project text logo" />
-            </a>
+      <Container className="inner">
+        <Row>
+          <Col sm={{ span: 3, order: 1 }} md={{ span: 12, order: 1 }} lg={{ span: 3, order: 1 }}>
+            <div className="widget">
+              <Image
+                src="/logos/kanji-text-white2.svg"
+                alt="Ukemi Project logo"
+                className="footer-logo"
+              />
+            </div>
+          </Col>
+
+          <Col className="col-6" md={3} lg={{ span: 2, offset: 1, order: 2 }}>
+            <div className="widget">
+              <h5 className="footer-title">Explore</h5>
+              <ul className="list-unstyled mb-0">
+                <li>
+                  <Link to="/" className="footer-link">
+                    Home
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/about" className="footer-link">
+                    About
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/services" className="footer-link">
+                    Services
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/shop" className="footer-link">
+                    Shop
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/blog" className="footer-link">
+                    Blog
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/contact" className="footer-link">
+                    Contact Us
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </Col>
+
+          <Col className="col-6" md={3} lg={{ span: 2, order: 3 }}>
+            <div className="widget">
+              <h5 className="footer-title">Projects</h5>
+              <ul className="list-unstyled mb-0">
+                {projects.length >= 1
+                  ? /* prettier-ignore */
+                    projects.map(({ node }) => (
+                      <li key={node.id}>
+                        <Link
+                          to={`/projects/${node.frontmatter.slug}`}
+                          className="footer-link">
+                          {node.frontmatter.name}
+                        </Link>
+                      </li>
+                  ))
+                  : null}
+              </ul>
+            </div>
+          </Col>
+
+          <Col className="col-6" md={3} lg={{ span: 2, order: 4 }}>
+            <div className="widget">
+              <h5 className="footer-title">Follow</h5>
+              <ul className="list-unstyled mb-0">
+                {userLinks.length > 0
+                  ? /* prettier-ignore */
+                    userLinks.map(( item ) => (
+                      <li key={item.label}>
+                        <a
+                          href={item.url}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          className="footer-link">
+                          {item.label}
+                        </a>
+                      </li>
+                  ))
+                  : null}
+              </ul>
+            </div>
+          </Col>
+
+          <Col className="col-6" md={3} lg={{ span: 2, order: 5 }}>
+            <div className="widget">
+              <h5 className="footer-title">Follow</h5>
+              <ul className="list-unstyled mb-0">
+                {userLinks.length > 0
+                  ? /* prettier-ignore */
+                    userLinks.map(( item ) => (
+                      <li key={item.label}>
+                        <a
+                          href={item.url}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          className="footer-link">
+                          {item.label}
+                        </a>
+                      </li>
+                  ))
+                  : null}
+              </ul>
+            </div>
+          </Col>
+
+          <Col sm={{ span: 12, order: 2 }} md={{ span: 12, order: 2 }} lg={{ span: 12, order: 6 }}>
             <p className="footer-slogan">
               <span className="text-white">受け身</span>
               <br />
               Movement inspired design specialists.
             </p>
-          </div>
-
-          <div className="footer-col">
-            <div className="footer-col-wrap">
-              <h6 className="footer-col-title">Explore</h6>
-              <div className="footer-nav-list">
-                <a href="/" className="footer-link" data-footer-link="1">
-                  Home
-                </a>
-                <a href="/about" className="footer-link" data-footer-link="2">
-                  About
-                </a>
-                <a href="/services" className="footer-link" data-footer-link="3">
-                  Services
-                </a>
-                <a href="/shop" className="footer-link" data-footer-link="4">
-                  Shop
-                </a>
-                <a href="/blog" className="footer-link" data-footer-link="5">
-                  Blog
-                </a>
-                <a href="/contact" className="footer-link" data-footer-link="6">
-                  Contact
-                </a>
-              </div>
-            </div>
-          </div>
-
-          <div className="footer-col">
-            <div className="footer-col-wrap">
-              <h6 className="footer-col-title">Projects</h6>
-              <div className="footer-nav-list">
-                {projects.length >= 1
-                  ? /* prettier-ignore */
-                    projects.map(({ node }) => (
-                      <Link
-                        key={node.id}
-                        to={`/projects/${node.frontmatter.slug}`}
-                        className="footer-link">
-                        {node.frontmatter.name}
-                      </Link>
-                  ))
-                  : null}
-              </div>
-            </div>
-          </div>
-
-          <div className="footer-col">
-            <div className="footer-col-wrap">
-              <h6 className="footer-col-title">Follow</h6>
-              {userLinks.length > 0
-                ? /* prettier-ignore */
-                  userLinks.map(( item ) => (
-                    <a
-                      key={item.label}
-                      href={item.url}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      className="footer-link">
-                      {item.label}
-                    </a>
-                  ))
-                : null}
-            </div>
-          </div>
-
-          <small className="footer-legal">{copyright}</small>
-        </Nav>
+            <small className="footer-legal">{copyright}</small>
+          </Col>
+        </Row>
       </Container>
     </footer>
   );
