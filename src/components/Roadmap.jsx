98074ecefb85d96a95664d62e9e7a090ea3fc802
Daniel Smith <headhertz88@gmail.com>
2020-02-24 20:55:44 +0000

    simpler roadmap implementation

--- src/components/Roadmap.jsx
+++ src/components/Roadmap.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useStaticQuery, graphql } from "gatsby";
 import Img from "gatsby-image";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Figure, Accordion, Button, Card } from "react-bootstrap";
 
 const Roadmap = () => {
   const data = useStaticQuery(graphql`
@@ -40,104 +40,96 @@ const Roadmap = () => {
   return (
     <section className="roadmap">
       <Container className="inner">
-        <Row>
-          <Col md={12}>
-            <div className="block-title">
-              <h2 className="block-title-text roadmap-title">Our 2020 Roadmap</h2>
-              <div className="desc">
-                <p>
-                  Vestibulum cursus felis sed massa tempus rutrum. Aenean rutrum elit non quam
-                  pharetra blandit. Aliquam
-                  <br />
-                  fringilla arcu magna, ut ultrices eros pharetra non. Sed at sapien vel nisi
-                  blandit molestie.
-                </p>
-              </div>
-            </div>
+        <Row className="align-items-center">
+          <Col lg={{ span: 6, order: 2 }} className="text-center">
+            <Figure>
+              <Figure.Image
+                src="images/text-logo272196-P5IYZO-590@2x.png"
+                fluid={false}
+                className="roadmap-img"
+              />
+            </Figure>
+          </Col>
+          <Col lg={6} className="pr-60 pr-md-15">
+            <h2 className="title">We've broken down our roadmap for 2020 into 3 stages.</h2>
+
+            <Accordion defaultActiveKey="0" className="simple">
+              <Card>
+                <Card.Header>
+                  <h5 className="title">
+                    <Accordion.Toggle as={Button} variant="link" eventKey="0">
+                      Stage 1
+                    </Accordion.Toggle>
+                  </h5>
+                </Card.Header>
+                <Accordion.Collapse eventKey="0">
+                  <Card.Body>
+                    <p>
+                      Step 1 on our roadmap was to gain meaningful traction and create authority in
+                      our space. Ukemi, therefore, began its life by developing products and
+                      spectacles by and for the movement community. Notably, to date, we have
+                      designed, built, programmed, managed and delivered Scotland’s largest ever
+                      parkour event as well as brought in the new year with our site-specific show
+                      SHINE by our in-house performance team.
+                    </p>
+                    <p>
+                      We have also released the Ukemi card game, the world’s first parkour card game
+                      - used by coaches worldwide in over 25 countries.
+                    </p>
+                  </Card.Body>
+                </Accordion.Collapse>
+              </Card>
+              <Card>
+                <Card.Header>
+                  <h5 className="title">
+                    <Accordion.Toggle as={Button} variant="link" eventKey="1">
+                      Stage 2
+                    </Accordion.Toggle>
+                  </h5>
+                </Card.Header>
+                <Accordion.Collapse eventKey="1">
+                  <Card.Body>
+                    <p>
+                      Step 2 on our roadmap bridges the gap between making ourselves known and
+                      designing activity landscapes that have an impact on cities across Scotland.
+                      At Ukemi, we leave no stone unturned while pursuing our goals. We realise that
+                      if we are to impact urban space, we would need to lay the foundation in
+                      individuals legal right to the city.
+                    </p>
+                    <p>
+                      With our Movement Card, we do just that. The Movement Card is a wallet-sized
+                      guide providing information to individuals on their rights to the city.
+                      Intended for urban sports enthusiasts, artists and activists primarily, it can
+                      also be beneficial to anyone who has interactions with them, such as security
+                      guards or the general public.
+                    </p>
+                  </Card.Body>
+                </Accordion.Collapse>
+              </Card>
+              <Card>
+                <Card.Header>
+                  <h5 className="title">
+                    <Accordion.Toggle as={Button} variant="link" eventKey="2">
+                      Stage 3
+                    </Accordion.Toggle>
+                  </h5>
+                </Card.Header>
+                <Accordion.Collapse eventKey="2">
+                  <Card.Body>
+                    <p>
+                      The ‘Movement Card’ will improve legislation. The Parkour History and Heritage
+                      Project will add authority and legitimise the spaces. The ‘Greatest Park Never
+                      Built’ project will document and collate the information into 3D assets. Then
+                      Ukemi will enter stage 3 and create the most magnificent movement landscapes
+                      ever seen, the most inclusive cities and fundamentally change the world we
+                      live in forever.
+                    </p>
+                  </Card.Body>
+                </Accordion.Collapse>
+              </Card>
+            </Accordion>
           </Col>
         </Row>
-        <div className="flat-divider" />
-        <div className="flat-steps-taken">
-          <Row>
-            <Col md={{ span: 4, offset: 1 }}>
-              <div className="steps-img left-img">
-                <Img fixed={data.one.childImageSharp.fixed} alt="Stage one" />
-              </div>
-            </Col>
-            <Col md={{ span: 4, offset: 2 }}>
-              <div className="steps-taken v1">
-                <h5 className="steps-title">
-                  Step 1: <span>Get Plan</span>
-                </h5>
-                <p className="steps-content">
-                  Proin et ipsum non neque interdum luctus a eu risus. Cras dolor justo, congue vel
-                  massa in, eleifend dapibus elit. Vivamus in tincidunt justo, in auctor odio.
-                  Vivamus a ullamcorper enim, et gravida nibh
-                </p>
-              </div>
-            </Col>
-          </Row>
-          <div className="flat-divider" />
-          <Row>
-            <Col md={{ span: 4, offset: 1 }}>
-              <div className="steps-taken">
-                <h5 className="steps-title">
-                  Step 2: <span>Development</span>
-                </h5>
-                <p className="steps-content">
-                  Proin et ipsum non neque interdum luctus a eu risus. Cras dolor justo, congue vel
-                  massa in, eleifend dapibus elit. Vivamus in tincidunt justo, in auctor odio.
-                  Vivamus a ullamcorper enim, et gravida nibh
-                </p>
-              </div>
-            </Col>
-            <Col md={{ span: 4, offset: 2 }}>
-              <div className="steps-img right-img">
-                <Img fixed={data.two.childImageSharp.fixed} alt="Stage two" />
-              </div>
-            </Col>
-          </Row>
-          <div className="flat-divider" />
-          <Row>
-            <Col md={{ span: 4, offset: 1 }}>
-              <div className="steps-img left-img">
-                <Img fixed={data.three.childImageSharp.fixed} alt="Stage three" />
-              </div>
-            </Col>
-            <Col md={{ span: 4, offset: 2 }}>
-              <div className="steps-taken">
-                <h5 className="steps-title">
-                  Step 3: <span>Test</span>
-                </h5>
-                <p className="steps-content">
-                  Proin et ipsum non neque interdum luctus a eu risus. Cras dolor justo, congue vel
-                  massa in, eleifend dapibus elit. Vivamus in tincidunt justo, in auctor odio.
-                  Vivamus a ullamcorper enim, et gravida nibh
-                </p>
-              </div>
-            </Col>
-          </Row>
-          <div className="flat-divider" />
-          <Row>
-            <Col md={{ span: 4, offset: 1 }}>
-              <div className="steps-taken v2">
-                <h5 className="steps-title">
-                  Step 4: <span>Launch</span>
-                </h5>
-                <p className="steps-content">
-                  Proin et ipsum non neque interdum luctus a eu risus. Cras dolor justo, congue vel
-                  massa in, eleifend dapibus elit. Vivamus in tincidunt justo, in auctor odio.
-                  Vivamus a ullamcorper enim, et gravida nibh
-                </p>
-              </div>
-            </Col>
-            <Col md={{ span: 4, offset: 2 }}>
-              <div className="steps-img right-img">
-                <Img fixed={data.four.childImageSharp.fixed} alt="Stage four" />
-              </div>
-            </Col>
-          </Row>
-        </div>
       </Container>
     </section>
   );
