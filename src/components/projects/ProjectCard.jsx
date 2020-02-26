dd6d758a59ac65df1f3207d402f9d40ec0b66ea6
Daniel Smith <headhertz88@gmail.com>
2020-02-26 14:23:48 +0000

    update project overview cards

--- src/components/projects/ProjectCard.jsx
+++ src/components/projects/ProjectCard.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Link } from "gatsby";
-import { Card, Col } from "react-bootstrap";
+import { Card, Col, Figure } from "react-bootstrap";
 
 const ProjectCard = ({ project }) => {
   return (
     <Col lg={4} md={6}>
-      <Card className="project-single">
+      {/* <Card className="project-single">
         <div className="project-single-image">
           {project.project_image && (
             <Link to={`projects/${project.slug}`}>
@@ -25,12 +25,27 @@ const ProjectCard = ({ project }) => {
             <h3>{project.name}</h3>
           </Link>
 
-          <Card.Subtitle className="project-single-meta">{/*  */}</Card.Subtitle>
+          <Card.Subtitle className="project-single-meta"></Card.Subtitle>
 
           <Card.Text>{project.objective}</Card.Text>
           <Card.Link href={`${project.slug}`}>Read More →</Card.Link>
         </Card.Body>
-      </Card>
+      </Card> */}
+
+      <div className="box shadow p-30">
+        <Figure className="main overlay">
+          <Link to={`projects/${project.slug}`}>
+            <Figure.Image rounded src={project.project_image} alt={project.name} />
+          </Link>
+        </Figure>
+        <div className="box-content">
+          <Link to={`projects/${project.slug}`}>
+            <h2 className="box-title mb-10">{project.name}</h2>
+          </Link>
+          <p>{project.objective}</p>
+          <Link to={`projects/${project.slug}`}>View Full Brief</Link>
+        </div>
+      </div>
     </Col>
   );
 };
