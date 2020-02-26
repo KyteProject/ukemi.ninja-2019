7a4446dda5ca2d198dc0e1a93da562445b35790e
Daniel Smith <headhertz88@gmail.com>
2020-02-26 14:24:40 +0000

    clean up commented code

--- src/components/projects/ProjectCard.jsx
+++ src/components/projects/ProjectCard.jsx
@@ -1,37 +1,10 @@
 import React from "react";
 import { Link } from "gatsby";
-import { Card, Col, Figure } from "react-bootstrap";
+import { Col, Figure } from "react-bootstrap";
 
 const ProjectCard = ({ project }) => {
   return (
     <Col lg={4} md={6}>
-      {/* <Card className="project-single">
-        <div className="project-single-image">
-          {project.project_image && (
-            <Link to={`projects/${project.slug}`}>
-              <Card.Img
-                variant="top"
-                src={project.project_image.replace("/static", "")}
-                alt={project.name}
-              />
-            </Link>
-          )}
-        </div>
-
-        <Card.Body className="project-single-content">
-          <span className="project-single-cat">{project.category}</span>
-
-          <Link to={`projects/${project.slug}`} className="project-single-title">
-            <h3>{project.name}</h3>
-          </Link>
-
-          <Card.Subtitle className="project-single-meta"></Card.Subtitle>
-
-          <Card.Text>{project.objective}</Card.Text>
-          <Card.Link href={`${project.slug}`}>Read More →</Card.Link>
-        </Card.Body>
-      </Card> */}
-
       <div className="box shadow p-30">
         <Figure className="main overlay">
           <Link to={`projects/${project.slug}`}>
