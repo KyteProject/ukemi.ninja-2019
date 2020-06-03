06de9159987096a0ec66358da53b229109b3fc61
Daniel Smith <headhertz88@gmail.com>
2020-06-03 13:39:35 +0100

    fix bad slugs on build

--- src/components/projects/ProjectCard.jsx
+++ src/components/projects/ProjectCard.jsx
@@ -3,20 +3,22 @@ import { Link } from "gatsby";
 import { Col, Figure } from "react-bootstrap";
 
 const ProjectCard = ({ project }) => {
+  const url = `/project/${project.slug}`;
+
   return (
     <Col lg={4} md={6} className="gallery-item">
       <div className="box shadow p-30">
         <Figure className="main overlay">
-          <Link to={project.slug}>
+          <Link to={url}>
             <Figure.Image rounded src={project.project_image} alt={project.name} />
           </Link>
         </Figure>
         <div className="box-content">
-          <Link to={project.slug}>
+          <Link to={url}>
             <h2 className="box-title mb-10">{project.name}</h2>
           </Link>
           <p>{project.objective}</p>
-          <Link to={project.slug}>View Full Brief</Link>
+          <Link to={url}>View Full Brief</Link>
         </div>
       </div>
     </Col>
--- src/components/shop/ListItem.jsx
+++ src/components/shop/ListItem.jsx
@@ -5,21 +5,23 @@ import { Col, Figure } from "react-bootstrap";
 import { formatPrice } from "../../utils/cart-helpers";
 
 const ListItem = ({ item }) => {
+  const url = `/shop/${item.slug}`;
+
   return (
     <Col lg={4} md={6} className="gallery-item">
       <div className="box shadow p-30">
         <Figure className="main overlay">
-          <Link to={`shop/${item.slug}`}>
+          <Link to={url}>
             <Figure.Image rounded src={item.thumbnail} alt={item.name} />
           </Link>
         </Figure>
         <div className="box-content">
-          <Link to={`shop/${item.slug}`}>
+          <Link to={url}>
             <h2 className="box-title mb-10">{item.name}</h2>
           </Link>
           <p>{formatPrice(item.price, "GBP")}</p>
           <p>{item.short_description}</p>
-          <Link to={`shop/${item.slug}`}>View Item</Link>
+          <Link to={url}>View Item</Link>
         </div>
       </div>
     </Col>
