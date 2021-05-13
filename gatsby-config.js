3cb008f780b6231181a8898dcf617cf73986a84a
KyteProject <headhertz88@gmail.com>
2021-05-13 12:07:17 +0100

    comment out google analytics due to not being used

--- gatsby-config.js
+++ gatsby-config.js
@@ -92,12 +92,12 @@ module.exports = {
         },
       },
     },
-    {
-      resolve: "gatsby-plugin-google-analytics",
-      options: {
-        trackingId: config.googleAnalyticsID,
-      },
-    },
+    // {
+    //   resolve: "gatsby-plugin-google-analytics",
+    //   options: {
+    //     trackingId: config.googleAnalyticsID,
+    //   },
+    // },
     {
       resolve: "gatsby-plugin-google-fonts",
       options: {
